32f4d9052341f60ad500129f7342b57d7a4299c0
sivasankar88 <sivasankar8383@gmail.com>
2026-03-11 20:48:02 +0530

    Initial commit

---
+++ src/main/java/com/market/gandhi_market_price_tracker_api/GandhiMarketPriceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.market.gandhi_market_price_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GandhiMarketPriceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GandhiMarketPriceTrackerApiApplication.class, args);
+	}
+
+}
